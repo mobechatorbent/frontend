8c1eac944d5fd37b2e9a9e6c519d162d1f956623
WooJun Kim <kwj2870@gmail.com>
2022-03-07 10:42:52 +0900

    Update source

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from baseline import *
 import pickle
 from flask import Flask, render_template
+import uuid
 
 app = Flask(__name__)
 
@@ -12,25 +13,24 @@ def index():
 def mozart():
     with open('Mozart.pkl', 'rb') as f:
         generator = pickle.load(f)
-    generator.eval()
-    sample_latent = torch.randn(n_samples, latent_dim)
-    if torch.cuda.is_available():
-        generator = generator.cuda()
-        sample_latent = sample_latent.cuda()
-    samples = generator(sample_latent).cpu().detach().numpy()
-    samples = samples.transpose(1, 0, 2, 3).reshape(n_tracks, -1, n_pitches)
-    tracks = []
-    for idx, (program, is_drum, track_name) in enumerate(zip(programs, is_drums, track_names)):
-        pianoroll = np.pad(samples[idx] > 0.5,((0, 0), (lowest_pitch, 128 - lowest_pitch - n_pitches)))
-        tracks.append(BinaryTrack(name=track_name,program=program,is_drum=is_drum,pianoroll=pianoroll))
-    m = Multitrack(tracks=tracks,tempo=tempo_array,resolution=beat_resolution)
-    m.write('static/mozart.mid')
-    return render_template("index.html", cname='mozart')
+    filename = generate(generator)
+    return render_template("index.html", cname='mozart', filename=filename)
 
 @app.route('/beethoven')
 def beethoven():
     with open('Beethoven.pkl', 'rb') as f:
         generator = pickle.load(f)
+    filename = generate(generator)
+    return render_template("index.html", cname='beethoven', filename=filename)
+
+@app.route('/others')
+def others():
+    with open('Classical_Era_further_epoch_10000.pkl', 'rb') as f:
+        generator = pickle.load(f)
+    filename = generate(generator)
+    return render_template("index.html", cname='others', filename=filename)
+
+def generate(generator):
     generator.eval()
     sample_latent = torch.randn(n_samples, latent_dim)
     if torch.cuda.is_available():
@@ -43,8 +43,10 @@ def beethoven():
         pianoroll = np.pad(samples[idx] > 0.5,((0, 0), (lowest_pitch, 128 - lowest_pitch - n_pitches)))
         tracks.append(BinaryTrack(name=track_name,program=program,is_drum=is_drum,pianoroll=pianoroll))
     m = Multitrack(tracks=tracks,tempo=tempo_array,resolution=beat_resolution)
-    m.write('static/beethoven.mid')
-    return render_template("index.html", cname='beethoven')
+    filename = f'{uuid.uuid4()}.mid'
+    m.write(f'static/{filename}')
+    return filename
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
